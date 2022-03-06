05effdf341d33936347cd3b06a3a18e16769ec5f
amitu2016 <amitu2016@gmail.com>
2022-03-06 15:32:51 +0530

    Triangle Pattern

---
+++ Coding Ninjas Assignments/src/patterns/TrianglePattern2.java
@@ -0,0 +1,28 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class TrianglePattern2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int i = 1;
+		int p = 1;
+		while(i <= n) {
+			int j = 1;
+			while(j <= i) {
+				System.out.print(p);
+				j++;
+				p++;
+			}
+			System.out.println();
+			i++;
+		}
+		sc.close();
+	}
+
+}
---
+++ Coding Ninjas Assignments/src/patterns/TrianglePatternAssignment3.java
@@ -0,0 +1,27 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class TrianglePatternAssignment3 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int i = 1;
+		
+		while(i <= n) {
+			int j = i;
+			while(j >= 1) {
+				System.out.print(j);
+				j--;
+			}
+			System.out.println();
+			i++;
+		}
+		sc.close();
+	}
+
+}
