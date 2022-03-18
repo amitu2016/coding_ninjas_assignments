b8748c04e383afc6579fd59c3726b962c41467ce
Amit Upadhyay <amitu@cdac.in>
2022-03-18 17:53:11 +0530

    recursion

---
+++ Coding Ninjas Assignments/src/recursion/NumberOfDigits.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class NumberOfDigits {
+	
+	public static int count(int n){
+		
+		if(n / 10 == 0) {
+			return 1;
+		}
+		
+		return 1 + count(n / 10);
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println(count(123));
+
+	}
+
+}
