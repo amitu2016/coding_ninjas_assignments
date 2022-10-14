ea08d6e6a8e39f7bfe818fcc9321621bcd4af0d9
Amit Upadhyay <amitu2016@gmail.com>
2022-10-15 01:35:43 +0530

    tree2

---
+++ Coding Ninjas Assignments/src/trees2/MirrorBinaryTree.java
@@ -0,0 +1,23 @@
+package trees2;
+
+public class MirrorBinaryTree {
+	
+	public static void mirrorBinaryTree(BinaryTreeNode<Integer> root){
+		if(root == null) {
+			return;
+		}
+		
+		BinaryTreeNode<Integer> temp = root.left;
+		root.left = root.right;
+		root.right = temp;
+		mirrorBinaryTree(root.left);
+		mirrorBinaryTree(root.right);
+	}
+
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = RemoveLeafNodes.takeTreeInputBetter(true, 0, false);
+		mirrorBinaryTree(root);
+		RemoveLeafNodes.printTreeDetailed(root);
+	}
+
+}
