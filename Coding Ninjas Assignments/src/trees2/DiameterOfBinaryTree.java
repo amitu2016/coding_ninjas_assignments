a82542a6e3801f63bdb2d2fca41e64fb64eb55e8
Amit Upadhyay <amitu2016@gmail.com>
2022-10-17 21:17:26 +0530

    trees

---
+++ Coding Ninjas Assignments/src/trees2/DiameterOfBinaryTree.java
@@ -0,0 +1,41 @@
+package trees2;
+
+public class DiameterOfBinaryTree {
+
+	public static int diameterOfBinaryTree(BinaryTreeNode<Integer> root){
+		if(root == null) {
+			return 0;
+		}
+		
+		int leftHeight = height(root.left);
+		int rightHeight = height(root.right);
+		
+		int diameter = leftHeight + rightHeight;
+		
+		int leftDiameter = diameterOfBinaryTree(root.left);
+		int rightDiameter = diameterOfBinaryTree(root.right);
+		
+		return 1 + Math.max(Math.max(leftDiameter, rightDiameter), diameter);
+		
+	}
+	
+	private static int height(BinaryTreeNode<Integer> root) {
+		if(root == null) {
+			return 0;
+		}
+		
+		int leftHeight = height(root.left);
+		int rightHeight = height(root.right);
+		
+		return 1 + Math.max(leftHeight, rightHeight);
+	}
+
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = RemoveLeafNodes.takeTreeInputBetter(true, 0, false);
+		int result = diameterOfBinaryTree(root);
+		
+		System.out.println("Diameter = "+result);
+
+	}
+
+}
