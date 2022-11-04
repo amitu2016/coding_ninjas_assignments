37d72286ea5d074e33f77d890a108c648b643656
Amit Upadhyay <amitu@cdac.in>
2022-11-04 19:28:18 +0530

    commit

---
+++ Coding Ninjas Assignments/src/binarysearchtree2/LargestBSTSubtree.java
@@ -0,0 +1,15 @@
+package binarysearchtree2;
+
+public class LargestBSTSubtree {
+	
+	public static int largestBSTSubtree(BinaryTreeNode<Integer> root) {
+		return 0;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
