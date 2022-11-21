efb860bf113e1f4a62df66ed17ec6b77d737203f
Amit Upadhyay <amitu@cdac.in>
2022-11-21 17:12:58 +0530

    dynamic programming started

---
+++ Coding Ninjas Assignments/src/dynamicprogramming1/Fibonnaci.java
@@ -0,0 +1,52 @@
+package dynamicprogramming1;
+
+import java.util.Scanner;
+
+public class Fibonnaci {
+	
+	public static int fibb(int n, int[] dp) {
+		
+		if(n == 0 || n == 1) {
+			return n;
+		}
+		
+		int ans1, ans2;
+		
+		if(dp[n -1] == -1) {
+			ans1 = fibb(n -1, dp);
+			dp[n -1] = ans1;
+		}else {
+			ans1 = dp[n - 1];
+		}
+		
+		if(dp[n -2] == -1) {
+			ans2 = fibb(n -2, dp);
+			dp[n -2] = ans2;
+		}else {
+			ans2 = dp[n - 2];
+		}
+		
+		int ans = ans1 + ans2;
+		
+		return ans;
+		
+	}
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			int n = sc.nextInt();
+			
+			int dp[] = new int[n];
+			
+			for (int i = 0; i < dp.length; i++) {
+				dp[i] = -1;
+			}
+			
+			int ans = fibb(n, dp);
+			
+			System.out.println(ans);
+		}
+
+	}
+
+}
